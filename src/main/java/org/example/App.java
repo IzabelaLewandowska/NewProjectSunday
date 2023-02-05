53209d173238b116bce695605e8b34e997c40ea0
lwndws <izalewandowska@outlook.com>
2023-02-05 13:46:38 +0100

    Implemented max function

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,11 +4,9 @@ package org.example;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 
     public static Integer min(Integer[] numbers) {
@@ -20,5 +18,16 @@ public class App
             }
         }
         return min;
+    }
+
+    public static Integer max(Integer[] numbers) {
+        Integer max = Integer.MIN_VALUE;
+
+        for (Integer num : numbers) {
+            if (num < min) {
+                max = num;
+            }
+        }
+        return max;
     }
 }
